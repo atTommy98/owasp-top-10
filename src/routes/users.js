bd1afd6521082becf847a1eb9d26bf2b0479fd81
atTommy98 <tommy_holt@icloud.com>
2026-07-20 17:20:40 +0100

    Added delete route

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,8 @@ import prisma from "#/db.js";
 const router = express.Router();
 
 router.get("/:id", async (req, res, next) => {
-  const id = Number(req.params.id)
+  const id = Number(req.params.id);
+  console.log(`Requested ID to GET: ${id}`);
   const user = await prisma.users.findUnique({
     where: {
       id,
@@ -15,16 +16,32 @@ router.get("/:id", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  console.log(req.body)
+  console.log(
+    `Creating new user: ${req.body.name} with email ${req.body.email}`,
+  );
+
+  // Currently anyone can create a user, only admins should be able to
   const user = await prisma.users.create({
     data: {
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
     },
   });
 
   res.json(user);
 });
+
+router.delete("/:id", async (req, res, next) => {
+  const id = Number(req.params.id);
+  console.log(`Requested ID to DEL: ${id}`);
+  const deletedUser = await prisma.users.delete({
+    where: {
+      id
+    }
+  })
+
+  res.json(deletedUser)
+})
 
 export default router;
