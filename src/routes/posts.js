f5f3304e193ace1b263291e9730259c9d3ed8d7d
atTommy98 <tommy_holt@icloud.com>
2026-07-28 15:34:19 +0100

    Added create Post route

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,6 +3,32 @@ import prisma from "#/db.js";
 
 const router = express.Router();
 
+// Create new post
+router.post("/", async (req, res, next) => {
+  // Check body exists
+  if (!req.body || typeof req.body !== "object") {
+    return res.status(404).json({ error: "Body required" });
+  }
+
+  const { authorId, content } = req.body;
+
+  const authorIdNum = Number(authorId);
+
+  if (typeof authorIdNum !== "number" || typeof content !== "string") {
+    return res.status(404).json({ error: "Invalid authorId or content types" });
+  }
+
+  const post = await prisma.posts.create({
+    data: {
+      authorId: authorIdNum,
+      content: content.trim(),
+    },
+  });
+
+  console.log("Post successfully created!");
+  return res.status(200).json({ msg: "Post successfully created!" });
+});
+
 router.get("/:id", async (req, res, next) => {
   const post = await prisma.posts.findUnique({ where: { id } });
   return res.send(post);
